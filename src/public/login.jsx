26d13492415f1fd833ca4c835a46e2f8685f8ded
baieric <ebai@uwaterloo.ca>
2015-02-22 09:55:45 -0500

    browserify.....

--- src/public/login.jsx
+++ src/public/login.jsx
@@ -1,15 +1,13 @@
 /*** @jsx REACT.DOM*/
 module.exports = React.createClass({
     render: function() {
-        return <div className="container">
-
+        return (<div className="container">
       <div className="jumbotron" id="login">
         <h1>Welcome to CrowdSurfr</h1>
         <p>Log in to join the open source playlist community.</p>
         <a href="/login" className="btn btn-primary btn-lg">Log in with Spotify</a>
-
       </div>
-    </div>;
+    </div>)
     }
 });
  
