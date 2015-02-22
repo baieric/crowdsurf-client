b1db620f5756a70017734923092c51aed4eac7e9
baieric <ebai@uwaterloo.ca>
2015-02-21 23:49:21 -0500

    implemented song search

--- src/public/playlistEdit.jsx
+++ src/public/playlistEdit.jsx
@@ -10,30 +10,193 @@ var playlist = {
 	"tracks": ["5Z7ygHQo02SUrFmcgpwsKW", "1x6ACsKV4UdWS2FMuPFUiT", "4bi73jCM02fMpkI11Lqmfe"]
 }
 
+var tracks = {
+	"tracks": []
+}
+
+/*
+var tracks = {
+  "tracks": [ {
+    "album": {
+      "album_type": "album",
+      "external_urls": {
+        "spotify": "https://open.spotify.com/album/6TJmQnO44YE5BtTxH8pop1"
+      },
+      "href": "https://api.spotify.com/v1/albums/6TJmQnO44YE5BtTxH8pop1",
+      "id": "6TJmQnO44YE5BtTxH8pop1",
+      "images": [ {
+        "height": 640,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/8e13218039f81b000553e25522a7f0d7a0600f2e",
+        "width": 629
+      }, {
+        "height": 300,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/8c1e066b5d1045038437d92815d49987f519e44f",
+        "width": 295
+      }, {
+        "height": 64,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/d49268a8fc0768084f4750cf1647709e89a27172",
+        "width": 63
+      } ],
+      "name": "Hot Fuss",
+      "type": "album",
+      "uri": "spotify:album:6TJmQnO44YE5BtTxH8pop1"
+    },
+    "artists": [ {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/0C0XlULifJtAgn6ZNCW2eu"
+      },
+      "href": "https://api.spotify.com/v1/artists/0C0XlULifJtAgn6ZNCW2eu",
+      "id": "0C0XlULifJtAgn6ZNCW2eu",
+      "name": "The Killers",
+      "type": "artist",
+      "uri": "spotify:artist:0C0XlULifJtAgn6ZNCW2eu"
+    } ],
+    "available_markets": [ "AD", "AR", "AT", "AU", "BE", "BG", "BO", "BR", "CH", "CL", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "EC", "EE", "ES", "FI", "FR", "GR", "GT", "HK", "HN", "HU", "IE", "IS", "IT", "LI", "LT", "LU", "LV", "MC", "MT", "MY", "NI", "NL", "NO", "NZ", "PA", "PE", "PH", "PL", "PT", "PY", "RO", "SE", "SG", "SI", "SK", "SV", "TR", "TW", "UY" ],
+    "disc_number": 1,
+    "duration_ms": 222075,
+    "explicit": false,
+    "external_ids": {
+      "isrc": "USIR20400274"
+    },
+    "external_urls": {
+      "spotify": "https://open.spotify.com/track/0eGsygTp906u18L0Oimnem"
+    },
+    "href": "https://api.spotify.com/v1/tracks/0eGsygTp906u18L0Oimnem",
+    "id": "0eGsygTp906u18L0Oimnem",
+    "name": "Mr. Brightside",
+    "popularity": 0,
+    "preview_url": "http://d318706lgtcm8e.cloudfront.net/mp3-preview/f454c8224828e21fa146af84916fd22cb89cedc6",
+    "track_number": 2,
+    "type": "track",
+    "uri": "spotify:track:0eGsygTp906u18L0Oimnem"
+  }, {
+    "album": {
+      "album_type": "album",
+      "external_urls": {
+        "spotify": "https://open.spotify.com/album/6TJmQnO44YE5BtTxH8pop1"
+      },
+      "href": "https://api.spotify.com/v1/albums/6TJmQnO44YE5BtTxH8pop1",
+      "id": "6TJmQnO44YE5BtTxH8pop1",
+      "images": [ {
+        "height": 640,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/8e13218039f81b000553e25522a7f0d7a0600f2e",
+        "width": 629
+      }, {
+        "height": 300,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/8c1e066b5d1045038437d92815d49987f519e44f",
+        "width": 295
+      }, {
+        "height": 64,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/d49268a8fc0768084f4750cf1647709e89a27172",
+        "width": 63
+      } ],
+      "name": "Hot Fuss",
+      "type": "album",
+      "uri": "spotify:album:6TJmQnO44YE5BtTxH8pop1"
+    },
+    "artists": [ {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/0C0XlULifJtAgn6ZNCW2eu"
+      },
+      "href": "https://api.spotify.com/v1/artists/0C0XlULifJtAgn6ZNCW2eu",
+      "id": "0C0XlULifJtAgn6ZNCW2eu",
+      "name": "The Killers",
+      "type": "artist",
+      "uri": "spotify:artist:0C0XlULifJtAgn6ZNCW2eu"
+    } ],
+    "available_markets": [ "AD", "AR", "AT", "AU", "BE", "BG", "BO", "BR", "CH", "CL", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "EC", "EE", "ES", "FI", "FR", "GR", "GT", "HK", "HN", "HU", "IE", "IS", "IT", "LI", "LT", "LU", "LV", "MC", "MT", "MY", "NI", "NL", "NO", "NZ", "PA", "PE", "PH", "PL", "PT", "PY", "RO", "SE", "SG", "SI", "SK", "SV", "TR", "TW", "UY" ],
+    "disc_number": 1,
+    "duration_ms": 197160,
+    "explicit": false,
+    "external_ids": {
+      "isrc": "USIR20400195"
+    },
+    "external_urls": {
+      "spotify": "https://open.spotify.com/track/1lDWb6b6ieDQ2xT7ewTC3G"
+    },
+    "href": "https://api.spotify.com/v1/tracks/1lDWb6b6ieDQ2xT7ewTC3G",
+    "id": "1lDWb6b6ieDQ2xT7ewTC3G",
+    "name": "Somebody Told Me",
+    "popularity": 0,
+    "preview_url": "http://d318706lgtcm8e.cloudfront.net/mp3-preview/4c63a3d4eaf7f8f86cfdb8bf46ef3974f4092357",
+    "track_number": 4,
+    "type": "track",
+    "uri": "spotify:track:1lDWb6b6ieDQ2xT7ewTC3G"
+  } ]
+ }
+
+ */
+
+var Track = React.createClass({
+
+	render: function(){
+		return (
+			<tr>
+				<td>{this.props.name}</td>
+				<td>{this.props.artist}</td>		
+				<td>{this.props.album}</td>
+			</tr>
+		)
+	}
+});
+
 var Playlist = React.createClass({
-	getInitialState: function(){
-		var tracks = [];
-		if(playlist){
-			var access_token = getCookie("access_token");
-			$.get('https://api.spotify.com/v1/tracks?ids='+playlist.tracks.slice(0, 50).join(),
+	addSong: function(event){
+		var that = this;
+		var access_token = "BQB3MEuAGTr9IoyP3SWFGUacE2uhKvHjkXy9k-ByZ_ZVuuiNGYRWV_iSosd74hB3jbbn_0HJHTzqBw-wZ5zjhITh5kRuDfFqWqp5YjxCR6b2tyMXqAzb-9vLE1Y0CH8JTfGGncwgFHQ0l06hlo9Y98mJBgRg5ag4wGcY5sq4RjS1ebJSZLQpzVAR6i9o4a5JobrL2YdzGq9Ksbk";
+		if(event.keyCode == 13){
+			var search = $('#songInput').val();
+			 $.get('https://api.spotify.com/v1/search?q='+ search + '&type=track&limit=5',
 				{ headers: {'Authorization': 'Bearer ' + access_token} },
 				function(result){
-					tracks = result;
-				});
+					var song = result.tracks.items[0];
+					console.log(song);
+					console.log(that.state);
+					that.state.tracks.push(song);
+					that.setState({tracks: that.state.tracks});
+					$('#songInput').val('');
+			});
+			
 		}
-
+	},
+	getInitialState: function(){
 		return {
 			playlist: playlist,
-			tracks: tracks
+			tracks: tracks.tracks
 		}
 	},
 	render: function(){
+		var that = this;
 		return (
 			<div className="container">
-				
+				<div className="input-group">
+					<input type="text" className="form-control" placeholder="Playlist Title"/>
+				</div>
+
+				<div className="input-group">
+					<input type="text" className="form-control" placeholder="Tags (separated by commas)"/>
+				</div>
+
+				<div className="input-group">
+					<input type="text" id="songInput" className="form-control" placeholder="Add songs" onKeyDown={that.addSong.bind(event)} />
+
+				</div>
+				<table className="table">
+					<tr>
+						<th>Track</th>
+						<th>Artist</th>		
+						<th>Album</th>
+					</tr>
+					{this.state.tracks.map(function (track){
+						return(
+							<Track name={track.name} artist={track.artists[0].name} album={track.album.name} />
+					)})}
+				</table>
+				<a href={"/save"} className="btn btn-primary">Save</a>
 			</div>
 		)
 	}
 })
+
 
 React.render(<Playlist playlist={playlist}></Playlist>, document.body);
--- src/public/playlistView.jsx
+++ src/public/playlistView.jsx
@@ -1,13 +1,4 @@
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
+
 
 // temp hardcoded playlist
 var playlist = {
@@ -22,38 +13,187 @@ var playlist = {
 	"tracks": ["5Z7ygHQo02SUrFmcgpwsKW", "1x6ACsKV4UdWS2FMuPFUiT", "4bi73jCM02fMpkI11Lqmfe"]
 }
 
+var tracks = {
+  "tracks": [ {
+    "album": {
+      "album_type": "album",
+      "external_urls": {
+        "spotify": "https://open.spotify.com/album/6TJmQnO44YE5BtTxH8pop1"
+      },
+      "href": "https://api.spotify.com/v1/albums/6TJmQnO44YE5BtTxH8pop1",
+      "id": "6TJmQnO44YE5BtTxH8pop1",
+      "images": [ {
+        "height": 640,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/8e13218039f81b000553e25522a7f0d7a0600f2e",
+        "width": 629
+      }, {
+        "height": 300,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/8c1e066b5d1045038437d92815d49987f519e44f",
+        "width": 295
+      }, {
+        "height": 64,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/d49268a8fc0768084f4750cf1647709e89a27172",
+        "width": 63
+      } ],
+      "name": "Hot Fuss",
+      "type": "album",
+      "uri": "spotify:album:6TJmQnO44YE5BtTxH8pop1"
+    },
+    "artists": [ {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/0C0XlULifJtAgn6ZNCW2eu"
+      },
+      "href": "https://api.spotify.com/v1/artists/0C0XlULifJtAgn6ZNCW2eu",
+      "id": "0C0XlULifJtAgn6ZNCW2eu",
+      "name": "The Killers",
+      "type": "artist",
+      "uri": "spotify:artist:0C0XlULifJtAgn6ZNCW2eu"
+    } ],
+    "available_markets": [ "AD", "AR", "AT", "AU", "BE", "BG", "BO", "BR", "CH", "CL", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "EC", "EE", "ES", "FI", "FR", "GR", "GT", "HK", "HN", "HU", "IE", "IS", "IT", "LI", "LT", "LU", "LV", "MC", "MT", "MY", "NI", "NL", "NO", "NZ", "PA", "PE", "PH", "PL", "PT", "PY", "RO", "SE", "SG", "SI", "SK", "SV", "TR", "TW", "UY" ],
+    "disc_number": 1,
+    "duration_ms": 222075,
+    "explicit": false,
+    "external_ids": {
+      "isrc": "USIR20400274"
+    },
+    "external_urls": {
+      "spotify": "https://open.spotify.com/track/0eGsygTp906u18L0Oimnem"
+    },
+    "href": "https://api.spotify.com/v1/tracks/0eGsygTp906u18L0Oimnem",
+    "id": "0eGsygTp906u18L0Oimnem",
+    "name": "Mr. Brightside",
+    "popularity": 0,
+    "preview_url": "http://d318706lgtcm8e.cloudfront.net/mp3-preview/f454c8224828e21fa146af84916fd22cb89cedc6",
+    "track_number": 2,
+    "type": "track",
+    "uri": "spotify:track:0eGsygTp906u18L0Oimnem"
+  }, {
+    "album": {
+      "album_type": "album",
+      "external_urls": {
+        "spotify": "https://open.spotify.com/album/6TJmQnO44YE5BtTxH8pop1"
+      },
+      "href": "https://api.spotify.com/v1/albums/6TJmQnO44YE5BtTxH8pop1",
+      "id": "6TJmQnO44YE5BtTxH8pop1",
+      "images": [ {
+        "height": 640,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/8e13218039f81b000553e25522a7f0d7a0600f2e",
+        "width": 629
+      }, {
+        "height": 300,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/8c1e066b5d1045038437d92815d49987f519e44f",
+        "width": 295
+      }, {
+        "height": 64,
+        "url": "https://d3rt1990lpmkn.cloudfront.net/original/d49268a8fc0768084f4750cf1647709e89a27172",
+        "width": 63
+      } ],
+      "name": "Hot Fuss",
+      "type": "album",
+      "uri": "spotify:album:6TJmQnO44YE5BtTxH8pop1"
+    },
+    "artists": [ {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/0C0XlULifJtAgn6ZNCW2eu"
+      },
+      "href": "https://api.spotify.com/v1/artists/0C0XlULifJtAgn6ZNCW2eu",
+      "id": "0C0XlULifJtAgn6ZNCW2eu",
+      "name": "The Killers",
+      "type": "artist",
+      "uri": "spotify:artist:0C0XlULifJtAgn6ZNCW2eu"
+    } ],
+    "available_markets": [ "AD", "AR", "AT", "AU", "BE", "BG", "BO", "BR", "CH", "CL", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "EC", "EE", "ES", "FI", "FR", "GR", "GT", "HK", "HN", "HU", "IE", "IS", "IT", "LI", "LT", "LU", "LV", "MC", "MT", "MY", "NI", "NL", "NO", "NZ", "PA", "PE", "PH", "PL", "PT", "PY", "RO", "SE", "SG", "SI", "SK", "SV", "TR", "TW", "UY" ],
+    "disc_number": 1,
+    "duration_ms": 197160,
+    "explicit": false,
+    "external_ids": {
+      "isrc": "USIR20400195"
+    },
+    "external_urls": {
+      "spotify": "https://open.spotify.com/track/1lDWb6b6ieDQ2xT7ewTC3G"
+    },
+    "href": "https://api.spotify.com/v1/tracks/1lDWb6b6ieDQ2xT7ewTC3G",
+    "id": "1lDWb6b6ieDQ2xT7ewTC3G",
+    "name": "Somebody Told Me",
+    "popularity": 0,
+    "preview_url": "http://d318706lgtcm8e.cloudfront.net/mp3-preview/4c63a3d4eaf7f8f86cfdb8bf46ef3974f4092357",
+    "track_number": 4,
+    "type": "track",
+    "uri": "spotify:track:1lDWb6b6ieDQ2xT7ewTC3G"
+  } ]
+}
+
+var author = {
+ "display_name" : "Lilla Namo",
+ "external_urls" : {
+ "spotify" : "https://open.spotify.com/user/tuggareutangranser"
+ },
+ "followers" : {
+ "href" : null,
+ "total" : 4561
+ },
+ "href" : "https://api.spotify.com/v1/users/tuggareutangranser",
+ "id" : "tuggareutangranser",
+ "images" : [ {
+ "height" : null,
+ "url" : "http://profile-images.scdn.co/artists/default/d4f208d4d49c6f3e1363765597d10c4277f5b74f",
+ "width" : null
+ } ],
+ "type" : "user",
+ "uri" : "spotify:user:tuggareutangranser"
+}
+
+var Track = React.createClass({
+
+	render: function(){
+		return (
+			<tr>
+				<td>{this.props.name}</td>
+				<td>{this.props.artist}</td>		
+				<td>{this.props.album}</td>
+			</tr>
+		)
+	}
+});
+
 var Playlist = React.createClass({
 	getInitialState: function(){
-		var access_token = getCookie("access_token");
-		var author;
-		var collaborators = [];
-		playlist.collaborators.map(function (collaborator) {
-			$.get('https://api.spotify.com/v1/users/'+collaborator.id,
-				{ headers: {'Authorization': 'Bearer ' + access_token} },
-				function(result){
-					var user = result;
-					collaborators.push(user);
-					if(collaborator.id == playlist.created_by){
-						author = user;
-					}
-			});
-		})
+		// access_token = "BQB3MEuAGTr9IoyP3SWFGUacE2uhKvHjkXy9k-ByZ_ZVuuiNGYRWV_iSosd74hB3jbbn_0HJHTzqBw-wZ5zjhITh5kRuDfFqWqp5YjxCR6b2tyMXqAzb-9vLE1Y0CH8JTfGGncwgFHQ0l06hlo9Y98mJBgRg5ag4wGcY5sq4RjS1ebJSZLQpzVAR6i9o4a5JobrL2YdzGq9Ksbk";
+		// var author;
+		// var collaborators = [];
+		// playlist.collaborators.map(function (collaborator) {
+		// 	$.get('https://api.spotify.com/v1/users/'+collaborator,
+		// 		{ headers: {'Authorization': 'Bearer ' + access_token} },
+		// 		function(result){
+		// 			var user = result;
+		// 			collaborators.push(user);
+		// 			if(collaborator == playlist.created_by){
+		// 				author = user;
+		// 			}
+		// 	},
 
-		var tracks = [];
-		$.get('https://api.spotify.com/v1/tracks?ids='+playlist.tracks.slice(0, 50).join(),
-			{ headers: {'Authorization': 'Bearer ' + access_token} },
-			function(result){
-				tracks = result;
-			});
+		// 	);
+		// })
+
+		// var tracks = [];
+		// $.get('https://api.spotify.com/v1/tracks?ids='+playlist.tracks.slice(0, 50).join(),
+		// 	{ headers: {'Authorization': 'Bearer ' + access_token} },
+		// 	function(result){
+		// 		tracks = result;
+		// 	});
+
+		// console.log(author);
+		console.log(tracks.tracks);
 
 		return {
 			playlist: playlist,
 			author: author,
-			collaborators: collaborators,
-			tracks: tracks
+			//collaborators: collaborators,
+			tracks: tracks.tracks
 		}
 	},
 	render: function(){
+		var count = 0;
 		return (
 			<div className="container">
 				<div className="inline">
@@ -80,7 +220,17 @@ var Playlist = React.createClass({
 					allowtransparency="true"
 					width="250" height="80">
 				</iframe>
-
+				<table className="table">
+					<tr>
+						<th>Track</th>
+						<th>Artist</th>		
+						<th>Album</th>
+					</tr>
+					{this.state.tracks.map(function (track){
+						return(
+							<Track name={track.name} artist={track.artists[0].name} album={track.album.name} />
+					)})}
+				</table>
 			</div>
 		)
 	}
